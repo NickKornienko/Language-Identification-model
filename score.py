d08d86069a148791a72c6601c4e7242d26f7dca8
Neel Desai <neelbhaveshdesai@gmail.com>
2023-12-14 18:22:14 -0800

    scoring script used by azure ml endpoint

---
+++ score.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+import os
+import logging
+import json
+import numpy
+
+
+def init():
+    """
+    This function is called when the container is initialized/started, typically after create/update of the deployment.
+    You can write the logic here to perform init operations like caching the model in memory
+    """
+    global model
+    # AZUREML_MODEL_DIR is an environment variable created during deployment.
+    # It is the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION)
+    # Please provide your model's folder name if there is one
+    model_path = os.path.join(
+        os.getenv("AZUREML_MODEL_DIR"),"255mode_5layerCNN.h5"
+    )
+    # deserialize the model file back into a sklearn model
+    model = tf.keras.models.load_model(model_path)
+    logging.info("Init complete")
+
+
+def run(raw_data):
+    """
+    This function is called for every invocation of the endpoint to perform the actual scoring/prediction.
+    In the example we extract the data from the json input and call the scikit-learn model's predict()
+    method and return the result back
+    """
+    try:
+        logging.info("model 1: request received")
+        data = json.loads(raw_data)["data"]
+        data = numpy.array(data)
+        logging.info(data.shape)
+        data = numpy.reshape(data, (1, 40, 1))
+        logging.info(data.shape)
+        result = model.predict(data)
+        logging.info("Request processed")
+        return result.tolist()
+    except Exception as e:
+        error = str(e)
+        logging.error(error)
+        return error
